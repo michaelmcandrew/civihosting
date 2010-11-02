5701efbbc59e4f73c7987bfeaa85e22e1069fdbc
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2010-11-02 17:40:53 +0000

    changed to more sensible directory permissions

--- backup_files.php
+++ backup_files.php
@@ -7,11 +7,10 @@ $clients=mysql_query("SELECT name FROM `client` ORDER BY 1 ASC");
 while($client=mysql_fetch_object($clients)){
 	$path="/var/www/{$client->name}/sites/default/files/";
 	if(file_exists($path)){
-		echo "$path exists\n";
 		$date=date('Ymd-Hi');
 		$backup_dir="/backup/clients/{$client->name}/files";
 		if(!file_exists($backup_dir)){
-			mkdir($backup_dir, 0777, 1);
+			mkdir($backup_dir, 0644, 1);
 		}
 		exec("rsync -aP --link-dest={$backup_dir}/latest {$path} {$backup_dir}/$date\n");
 		exec("rm -f {$backup_dir}/latest\n");
