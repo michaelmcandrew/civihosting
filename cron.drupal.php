68004ae5d22837aaade79c290d218542ac99402a
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-02-23 21:15:46 +0000

    updating cron

--- cron.drupal.php
+++ cron.drupal.php
@@ -4,10 +4,10 @@ $drupal_cron_clients=ch_query("SELECT client.name
 FROM client_cron
 JOIN client ON client.id = client_cron.client_id
 JOIN cron ON cron.id = client_cron.cron_id
-WHERE cron.name='drupal'");
+WHERE cron.name='drupal'", 'force');
 
 while($client=mysql_fetch_object($drupal_cron_clients)){
 	$exec[]="drush -r /var/www/{$client->name} core-cron";
 }
-exec(implode(";\n", $exec));
+ch_exec(implode(";\n", $exec));
 ?>
